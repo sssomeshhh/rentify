e2cd2fb5bff24cae69dd93009f79d79242516bed
SomesH S <sssomeshhh@gmail.com>
2024-05-28 09:30:30 +0000

    imports.js - remove pth, ses, url

--- be/src/helpers/imports.js
+++ be/src/helpers/imports.js
@@ -4,9 +4,6 @@ import cry from "crypto";
 import exp from "express";
 import jwt from "jsonwebtoken";
 import mng from "mongoose";
-import pth from "path";
-import ses from "express-session";
-import url from "url";
 
 export {
   bcr,
@@ -14,8 +11,5 @@ export {
   cry,
   exp,
   jwt,
-  mng,
-  pth,
-  ses,
-  url
+  mng
 };
